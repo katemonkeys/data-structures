9ed0a4ea9c44718cea46dd8b9fc361a38884695a
Kate Jenkins <catherine.ann.jenkins@gmail.com>
2013-09-25 15:34:20 -0700

    added "extend" call to makeStack with first three passing tests.

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,6 +1,44 @@
-var makeStack = function() {
-  // Hey! Copy your code from src/functional/stack.js and paste it here
+var makeStack = function(){
+  var instance = {};
+
+  // Use an object with numeric keys to store values
+  instance.storage = {};
+  instance.len = 0; // Hint: set an initial value here
+
+  extend(instance, makeStack.stackMethods);
+
+  return instance;
 };
 
-var stackMethods = {};
+var extend = function(to,from) {
+
+  for (var property in from) {
+    to[property] = from[property];
+  }
+};
+
+makeStack.stackMethods = {
+
+  push: function(value){
+    this.storage[this.len] = value;
+    this.len++;
+  },
+
+  pop: function(){
+
+    var element;
+    if (this.len) {
+      element = storage[this.len-1];
+      // delete storage[size];
+      this.len--;
+    }
+    return element;
+
+  },
+
+  size: function(){
+    return this.len;
+  }
+
+};
 
