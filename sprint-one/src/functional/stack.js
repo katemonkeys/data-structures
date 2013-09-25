9b477e9024fb38201f66d96ea44aa10ef67d97b9
Hack Reactor Students <github@hackreactor.com>
2013-09-25 14:43:27 -0700

    Stack and queue pass all tests in functional instantiation

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,17 +3,29 @@ var makeStack = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
-  var size; // Hint: set an initial value here
+  var size = 0; // Hint: set an initial value here
 
   // Implement the methods below
 
   instance.push = function(value){
+
+    storage[size] = value;
+    size++;
+
   };
 
   instance.pop = function(){
+    var element;
+    if (size) {
+      element = storage[size-1];
+      // delete storage[size];
+      size--;
+    }
+    return element;
   };
 
   instance.size = function(){
+    return size;
   };
 
   return instance;
