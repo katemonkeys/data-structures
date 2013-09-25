075cb24330a2942485f910ddd45cb2038fec0b97
Kate Jenkins <catherine.ann.jenkins@gmail.com>
2013-09-25 14:43:27 -0700

    Stack and queue pass all tests in functional instantiation

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -4,15 +4,28 @@ var makeQueue = function(){
   // Use an object with numeric keys to store values
   var storage = {};
 
+  var front = 0;
+  var back  = 0;
+
   // Implement the methods below
 
   instance.enqueue = function(value){
+
+    storage[back] = value;
+    back++;
+
   };
 
   instance.dequeue = function(){
+
+    var element = storage[front];
+    if (front<back) {front++;}
+    return element;
+
   };
 
   instance.size = function(){
+    return back - front;
   };
 
   return instance;
