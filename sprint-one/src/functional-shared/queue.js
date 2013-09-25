67fb45bdf8bea10219fd4a1b4e8cc077b3e8e13e
Hack Reactor Students <github@hackreactor.com>
2013-09-25 15:34:20 -0700

    added "extend" call to makeStack with first three passing tests.

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,6 +1,36 @@
 var makeQueue = function(){
-  // Hey! Copy your code from src/functional/queue.js and paste it here
+  var instance = {};
+
+  // Use an object with numeric keys to store values
+  var storage = {};
+
+  var front = 0;
+  var back  = 0;
+
+  // Implement the methods below
+
+  instance.enqueue = function(value){
+
+    storage[back] = value;
+    back++;
+
+  };
+
+  instance.dequeue = function(){
+
+    var element = storage[front];
+    if (front<back) {front++;}
+    return element;
+
+  };
+
+  instance.size = function(){
+    return back - front;
+  };
+
+  return instance;
 };
+
 
 var queueMethods = {};
 
