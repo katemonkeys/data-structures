5a84ae604cf17d3be4a01f5a1273f745901d02b1
Kate Jenkins <catherine.ann.jenkins@gmail.com>
2013-09-25 17:25:17 -0700

    All queue tests pass on prototypal section. Hooray.

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,6 +1,36 @@
-var makeQueue = function() {
-  // Hey! Copy your code from src/functional-shared/queue.js and paste it here
+var makeQueue = function(){
+  var instance = Object.create(queueMethods);
+
+  // Use an object with numeric keys to store values
+  instance.storage = {};
+
+  instance.front = 0;
+  instance.back  = 0;
+
+  return instance;
 };
 
-var queueMethods = {};
+
+var queueMethods = {
+
+  enqueue: function(value){
+
+    this.storage[this.back] = value;
+    this.back++;
+
+  },
+
+  dequeue: function(){
+
+    var element = this.storage[this.front];
+    if (this.front<this.back) {this.front++;}
+    return element;
+
+  },
+
+  size: function(){
+    return this.back - this.front;
+  }
+
+};
 
