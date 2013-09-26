82f219937dcbbb82522b4cc49d33f3f2629771a2
Ben Martin <tennisracket22@yahoo.com>
2013-09-26 10:24:38 -0700

    Finished LinkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,18 +1,41 @@
-// Note: don't use an array to do this.
-var makeLinkedList = function(){
+var makeLinkedList = function(){	
+
   var list = {};
-  list.head = null;
-  list.tail = null;
+  list.head 	= null;
+  list.tail 	= null;
 
-  list.addToTail = function(){
+  list.addToTail = function(value){
+  
+  	var node = makeNode(value);
+
+  	(list.tail ? list.tail.next = node : list.head = node);
+  	
+  	list.tail = node;
   };
-
+  
   list.removeHead = function(){
+  
+  	var headNode = list.head;
+  	
+  	list.head = list.head.next;
+  	
+  	return headNode;
   };
 
-  list.contains = function(){
+  list.contains = function(value){
+  
+		var node = list.head;
+  
+  	while (node) {
+	  	if (node.value === value)
+	  		return true;
+	  		
+			node = node.next;
+  	}
+  	
+  	return false;
   };
-
+  
   return list;
 };
 
