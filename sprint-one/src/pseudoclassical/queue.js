f07c748cfafc32f0a322a7095e04332f6d425875
Kate Jenkins <catherine.ann.jenkins@gmail.com>
2013-09-25 17:43:26 -0700

    Pseudoclassical Stacks!!!

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,4 +1,37 @@
-var Queue = function() {
-  // Hey! Copy your code from src/prototypal/queue.js and paste it here
+var Queue = function(){
+  var instance = Object.create(queueMethods);
+
+  // Use an object with numeric keys to store values
+  instance.storage = {};
+
+  instance.front = 0;
+  instance.back  = 0;
+
+  return instance;
 };
+
+
+var queueMethods = {
+
+  enqueue: function(value){
+
+    this.storage[this.back] = value;
+    this.back++;
+
+  },
+
+  dequeue: function(){
+
+    var element = this.storage[this.front];
+    if (this.front<this.back) {this.front++;}
+    return element;
+
+  },
+
+  size: function(){
+    return this.back - this.front;
+  }
+
+};
+
 
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,4 +1,23 @@
-var Stack = function() {
-  // Hey! Copy your code from src/prototypal/stack.js and paste it here
+var Stack = function(storage, len){
+  this.storage = storage || {};
+  this.len     = len     || 0;
+};
+
+Stack.prototype.push = function(value){
+  this.storage[this.len] = value;
+  this.len++;
+};
+
+Stack.prototype.pop = function(){
+
+  if (this.len) {
+    this.len--;
+    return this.storage[this.len];
+  }
+
+};
+
+Stack.prototype.size = function(){
+  return this.len;
 };
 
