61f05290f9a0e6b896599d157ce8c96043b2c979
Ben Martin <tennisracket22@yahoo.com>
2013-09-26 10:24:38 -0700

    Finished LinkedList

--- sprint-two/spec/linkedListSpec.js
+++ sprint-two/spec/linkedListSpec.js
@@ -15,6 +15,27 @@ describe("linkedList", function() {
     expect(linkedList.removeHead).toEqual(jasmine.any(Function));
     expect(linkedList.contains).toEqual(jasmine.any(Function));
   });
-
-  // add more tests here to test the functionality of linkedList
+  
+  it("takes a value and adds it to the end of the list", function(){
+	  linkedList.addToTail(1);
+	  expect(linkedList.tail.value).toEqual(1);
+  });
+  
+  it("removes the first node from the list and returns its value", function(){
+	  linkedList.addToTail(1);
+	  linkedList.addToTail(2);
+	  var node = linkedList.removeHead();
+	  expect(node.value).toEqual(1);	  
+  });
+  
+  it("returns boolean reflecting whether or not the passed-in value is in the linked list", function(){
+	  linkedList.addToTail(1);
+	  expect(linkedList.contains(1)).toEqual(true);
+	  
+	  linkedList.addToTail(2);
+	  expect(linkedList.contains(2)).toEqual(true);
+	  
+	  linkedList.removeHead();
+	  expect(linkedList.contains(1)).toEqual(false);
+  });
 });
