84b30d87f3bc77168255fafd669d46b6aeb68677
Hack Reactor Students <github@hackreactor.com>
2013-09-27 12:19:49 -0700

    LinkedList extra credit, added parent to tree, fixed hashtable when it referred to linkedList functions.

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,5 +1,5 @@
 var HashTable = function(){
-  this._limit = 3;
+  this._limit = 8;
 
   // Use a limited array to store inserted elements.
   // It'll keep you from using too much space. Usage:
@@ -44,41 +44,3 @@ HashTable.prototype.remove = function(k){
   }
 };
 
-
-// ----------------------------------------- //
-
-var makeLinkedList = function(){
-
-  var list = {};
-  list.head = null;
-  list.tail = null;
-  list.addToTail = function(value){
-    var node = makeNode(value);
-    list.tail ? list.tail.next = node : list.head = node;
-    list.tail = node;
-  };
-
-  list.removeHead = function(){
-    var headNode = list.head;
-    list.head = list.head.next;
-    return headNode;
-  };
-
-  list.contains = function(value){
-    var node = list.head;
-    while (node) {
-      if (node.value === value)
-        return true;
-      node = node.next;
-    }
-    return false;
-  };
-  return list;
-};
-
-var makeNode = function(value){
-  var node = {};
-  node.value = value;
-  node.next = null;
-  return node;
-};
