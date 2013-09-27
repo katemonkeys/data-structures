bb01748823a6b08f660fe15c800a92c1539e500f
Hack Reactor Students <github@hackreactor.com>
2013-09-26 19:36:24 -0700

    LinkedLists and Trees (all of them)

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,15 +1,37 @@
-var makeTree = function(){
+var makeTree = function(value){
   var newTree = {};
-  newTree.value = undefined;
-  newTree.children = undefined;
+  newTree.value = value;
+  newTree.children = [];
+
+  extend(newTree, treeMethods);
 
   return newTree;
 };
 
-var treeMethods = {};
-
-treeMethods.addChild = function(){
+var extend = function(to, from) {
+  for (var key in from)
+    to[key] = from[key];
 };
 
-treeMethods.contains = function(){
+var treeMethods = {};
+
+treeMethods.addChild = function(value){
+
+  var node = makeTree(value);
+
+  this.children.push(node);
+
+};
+
+treeMethods.contains = function(target){
+
+  if (this.value === target) {
+      return true;
+  }
+
+  for (var i = 0; i < this.children.length; i++) {
+    return this.children[i].contains(target);
+  }
+
+  return false;
 };
