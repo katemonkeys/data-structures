b2db3deb072d5783d3d2db84533300828e543405
Kate Jenkins <catherine.ann.jenkins@gmail.com>
2013-09-26 19:36:24 -0700

    LinkedLists and Trees (all of them)

--- sprint-two/spec/treeSpec.js
+++ sprint-two/spec/treeSpec.js
@@ -11,5 +11,30 @@ describe("tree", function() {
     expect('value' in tree).toBe(true);
   });
 
-  // Add more tests here to test the functionality of tree.
+  it("takes any value, sets that as the target of a node, and adds that node as a child of the tree", function() {
+    var tree  = makeTree(5);
+
+    tree.addChild(6);
+    expect(tree.children[0].value).toEqual(6);
+
+    tree.children[0].addChild(7);
+    expect(tree.children[0].children[0].value).toEqual(7);
+  });
+
+  it("takes any input and returns a boolean reflecting whether it can be found as the value of the target node or any descendant node", function(){
+    var root  = makeTree(1);
+
+    root.addChild(2);
+    root.addChild(3);
+
+    root.children[0].addChild(4);
+    root.children[1].addChild(4);
+
+    expect(root.value).toEqual(1);
+    expect(root.children[1].value).toEqual(3);
+    expect(root.contains(2)).toEqual(true);
+    expect(root.contains(4)).toEqual(true);
+    expect(root.contains(9.3)).toEqual(false);
+  });
+  
 });
--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,41 +1,41 @@
-var makeLinkedList = function(){	
+var makeLinkedList = function(){
 
   var list = {};
-  list.head 	= null;
-  list.tail 	= null;
+  list.head = null;
+  list.tail = null;
 
   list.addToTail = function(value){
-  
-  	var node = makeNode(value);
 
-  	(list.tail ? list.tail.next = node : list.head = node);
-  	
-  	list.tail = node;
+    var node = makeNode(value);
+
+    list.tail ? list.tail.next = node : list.head = node;
+
+    list.tail = node;
   };
-  
+
   list.removeHead = function(){
-  
-  	var headNode = list.head;
-  	
-  	list.head = list.head.next;
-  	
-  	return headNode;
+
+    var headNode = list.head;
+
+    list.head = list.head.next;
+
+    return headNode;
   };
 
   list.contains = function(value){
-  
-		var node = list.head;
-  
-  	while (node) {
-	  	if (node.value === value)
-	  		return true;
-	  		
-			node = node.next;
-  	}
-  	
-  	return false;
+
+    var node = list.head;
+
+    while (node) {
+      if (node.value === value)
+        return true;
+
+      node = node.next;
+    }
+
+    return false;
   };
-  
+
   return list;
 };
 
