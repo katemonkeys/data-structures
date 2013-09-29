7bad9e9779e0cdec8149e4af6de74fdfa180e983
Kate Jenkins <catherine.ann.jenkins@gmail.com>
2013-09-28 17:25:21 -0700

    Incorporated some of the classroom solution. Temporarily broken.

--- sprint-two/spec/hashTableSpec.js
+++ sprint-two/spec/hashTableSpec.js
@@ -20,8 +20,8 @@ describe("hashTable", function() {
     hashTable.insert(v4, v4);
 //overwhelm the hash table by setting limit to a small whole number when performing this test
 
-    expect(hashTable.retrieve(v1)).toEqual(v1);
-    expect(hashTable.retrieve(v2)).toEqual(v2);
+  expect(hashTable.retrieve(v1)).toEqual(v1);
+  expect(hashTable.retrieve(v2)).toEqual(v2);
   });
 
   it("should remove hash-value pairs", function(){
@@ -35,7 +35,22 @@ describe("hashTable", function() {
 
     expect(hashTable.retrieve(v1)).toEqual(undefined);
     expect(hashTable.retrieve(v2)).toEqual(v2);
-    
+
+  });
+
+  it("should dynamically resize when the density gets too high", function() {
+    var v1 = 'val1', v2 = 'val2', v3 = 'val3', v4 = 'val4';
+    var v5 = 'val5', v6 = 'val6', v7 = 'val7', v8 = 'val8';
+    hashTable.insert(v1, v1);
+    hashTable.insert(v3, v3);
+    hashTable.insert(v2, v2);
+    hashTable.insert(v4, v4);
+    hashTable.insert(v5, v5);
+    hashTable.insert(v6, v6);
+    hashTable.insert(v7, v7);
+    hashTable.insert(v8, v8);
+
+    expect(hashTable._limit).toEqual(16);
   });
 
 
